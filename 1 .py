50a3404915281a9021fe50493fc0de21cea52f0c
adityamankar <adityamankar022@gmail.com>
2026-04-02 12:51:16 +0530

    String Methods

---
+++ 1 .py
@@ -0,0 +1,24 @@
+#STRING METHODS 
+
+# lower()
+# upper()
+# title()
+# startwith()
+# endwith()
+# split()
+# join()
+# index()
+# join()
+# capitalize()
+
+s="this is python class"
+s=input("Enter name:")
+ch=input("Enter any char:")
+start=int(input("Enter start point:"))
+stop=int(input("Enter stop point:"))
+print(s.index(ch,start,stop))
+print(s.split())
+# print(s.upper())
+# print(s.lower())
+# print(s.swapcase())
+# print(s.title())
--- 1.py
+++
@@ -1,4 +0,0 @@
-#eval()
-
-x=eval(input("Enter any data"))
-print(x,type(x))
