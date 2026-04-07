71b67f2eb8d0398154cf9e440b766454c6cb2253
Aditya Mankar <adityamankar022@gmail.com>
2026-04-07 13:21:34 +0530

    Tuple & Count

---
+++ 3.py
@@ -0,0 +1,9 @@
+# index()
+
+l=evel(input("Enter any tuple: "))
+print(l.index(2)) #To find the index of an element
+
+# count
+
+l=eval(input("Enter any tuple and count: "))
+print(l.count(2)) #To find the occurance of the element
