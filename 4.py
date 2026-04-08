cc9a3a0aecd35a76ac683b8508964d8da037c1ad
Aditya Mankar <adityamankar022@gmail.com>
2026-04-08 13:16:05 +0530

    Dictionay

---
+++ 4.py
@@ -0,0 +1,31 @@
+# Dictionary
+
+# mutable in nature
+# collection of "key" : "value"
+
+
+d={
+'name':'person',
+'age':37,
+'quality':'Bhopal'
+}
+
+print(d)
+print(type(d))
+print(len(d))
+print(max(d))
+print(min(d))
+print(d)
+
+# Dict Methods
+
+# copy() create new object
+# clear() remove all pairs from dict
+# get() d.get('key') --> 'value'
+# keys() 
+# values()
+# items()
+# fromkeys()
+# update()
+# setdefault()
+# pop()
