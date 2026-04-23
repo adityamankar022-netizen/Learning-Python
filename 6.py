1caa59c46583ab9f3d15852721d7cb3b8d8291fb
Aditya Mankar <adityamankar022@gmail.com>
2026-04-23 13:12:43 +0530

    FOR LOOPS

---
+++ 6.py
@@ -0,0 +1,39 @@
+    # FOR LOOPS
+
+# l=[10,20,30,40]
+# l1=[]
+
+# for i in l:
+#     l1.append(i**2)
+# print(l1)
+
+# FOR TUPLE
+
+# l=[10,20,30,40]
+# t=(1,2,3,4,5,6)
+# l=list(l)
+# l1=[]
+# for i in l:
+#     l1.append(i+5)
+# t=tuple(l1)
+# print(t)
+# print(l)
+
+# #FOR LIST
+
+# l=[1,2,3,4,5]
+# for i in range(len(l)):
+#     x=l[i]+5
+#     l[i]=x
+# print(l)
+
+
+# FOR DICTIONARY 
+
+d={
+    'name':'person',
+    'age':30,
+    '2':'python'
+}
+for i in d:
+    print(i,'=>',d[i])
